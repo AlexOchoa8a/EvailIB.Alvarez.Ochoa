851837dda289e0c62695defba03bc54a94145681
AlexOchoa8a <61293955+AlexOchoa8a@users.noreply.github.com>
2020-03-12 14:49:18 -0600

    Add files via upload

---
+++ Mayor y menor .py
@@ -0,0 +1,17 @@
+num1= int(input("ingrese un numero"))
+num2 = int(input("ingrese segundo numero"))
+num3 = int(input("ingrese tercer numero"))
+
+if num1>num2:
+    print(num1)
+elif num2>num1:
+    print(num2)
+elif num1>num3:
+    print(num1)
+elif num3>num2:
+    print(num3)
+elif num3>num1:
+    print(num3)
+
+
+input()
---
+++ Promedio .py
@@ -0,0 +1,16 @@
+nota1 = int(input("Ingrese su nota por favor: "))
+nota2 = int(input("Ingrese la segunda nota por favor: "))
+nota3 = int(input("Ingrese la tercera  nota por favor: "))
+nota4 = int(input("Ingrese la cuarta nota  nota por favor: "))
+
+
+#La asentrencia para la ejecucion
+
+resultado = nota1+nota2+nota3+nota4/240
+
+print("El promedio del estudiante es de:",resultado)
+
+
+
+
+
